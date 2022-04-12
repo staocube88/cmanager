77f310d9eb1098360ee8c5372909c957eecf61c4
Oluwatobi Solomon <staocube@gmail.com>
2022-04-12 23:09:18 +0100

    dockerized commit

---
+++ src/admin/dbconn.php
@@ -0,0 +1,9 @@
+<?php
+$host="db";
+		$uname="root";
+		$pas="";
+		$db_name="cman";
+		$tbl_name="admin";
+		
+		$conn = @mysqli_connect("$host","$uname","$pas","$db_name") or die ("cannot connect to the database");
+		?>
